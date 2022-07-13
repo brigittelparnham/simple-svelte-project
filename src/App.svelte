3b6f5e38c37de805866a2040080d7758ba66561e
brigittelparnham <brigitte.parnham@gmail.com>
2022-07-13 10:27:04 +0100

    added routes

--- src/App.svelte
+++ src/App.svelte
@@ -1,65 +1,20 @@
 <script>
-  import logo from './assets/svelte.png'
-  import Counter from './lib/Counter.svelte'
+    import { Router, Route } from "svelte-routing";
+    import NavLink from "./components/NavLink.svelte";
+	import Home from "./routes/Home.svelte";
+	import Blog from "./routes/Blog.svelte";
+	
+	export let url = "";
 </script>
 
-<main>
-  <img src={logo} alt="Svelte Logo" />
-  <h1>Hello world!</h1>
+<Router url="{url}">
+	<nav>
+		<NavLink to="/">Home</NavLink>
+		<NavLink to="blog">Blog</NavLink>
+	</nav>
+	<div>
+		<Route path="blog/*" component="{Blog}"/>
+		<Route path="/" component="{Home}"/>
+	</div>
+</Router>
 
-  <Counter />
-
-  <p>
-    Visit <a href="https://svelte.dev">svelte.dev</a> to learn how to build Svelte
-    apps.
-  </p>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme">SvelteKit</a> for
-    the officially supported framework, also powered by Vite!
-  </p>
-</main>
-
-<style>
-  :root {
-    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
-      Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
-  }
-
-  main {
-    text-align: center;
-    padding: 1em;
-    margin: 0 auto;
-  }
-
-  img {
-    height: 16rem;
-    width: 16rem;
-  }
-
-  h1 {
-    color: #ff3e00;
-    text-transform: uppercase;
-    font-size: 4rem;
-    font-weight: 100;
-    line-height: 1.1;
-    margin: 2rem auto;
-    max-width: 14rem;
-  }
-
-  p {
-    max-width: 14rem;
-    margin: 1rem auto;
-    line-height: 1.35;
-  }
-
-  @media (min-width: 480px) {
-    h1 {
-      max-width: none;
-    }
-
-    p {
-      max-width: none;
-    }
-  }
-</style>
