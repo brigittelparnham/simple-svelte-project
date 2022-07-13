1440b2e56e3e5656107a163d5ea56d6f74e8f224
brigittelparnham <brigitte.parnham@gmail.com>
2022-07-13 13:52:44 +0100

    d3 added but not drawing

--- src/App.svelte
+++ src/App.svelte
@@ -3,7 +3,7 @@
     import NavLink from "./components/NavLink.svelte";
 	import Home from "./routes/Home.svelte";
 	import Blog from "./routes/Blog.svelte";
-	
+	import D3 from "./routes/D3.svelte";	
 	export let url = "";
 </script>
 
@@ -11,9 +11,11 @@
 	<nav>
 		<NavLink to="/">Home</NavLink>
 		<NavLink to="blog">Blog</NavLink>
+		<NavLink to="d3">D3</NavLink>
 	</nav>
 	<div>
 		<Route path="blog/*" component="{Blog}"/>
+		<Route path="d3" component="{D3}"/>
 		<Route path="/" component="{Home}"/>
 	</div>
 </Router>
--- src/routes/Home.svelte
+++ src/routes/Home.svelte
@@ -1,2 +1,3 @@
 <h1>Home</h1>
-<p>landing page</p>
+<p>Starting with svelte</p>
+
